1bc224a6ef0892671af01b1addd31f0124c92a63
BouncingElf10 <mathisbonne09@gmail.com>
2026-03-27 13:25:27 +0100

    updated to 1.5.0 version in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@ plugins {
 
 
 group = "com.bouncingelf10"
-version = "1.4.0"
+version = "1.5.0"
 
 gradlePlugin {
     website = "https://github.com/BouncingElf10/MinecraftVersionCompatibilityChecker"
